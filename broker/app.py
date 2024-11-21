b7708c9e251005d5fefe81f4a85c220d11e8defa
Chiara Mezzavilla <2512470+samurang87@users.noreply.github.com>
2024-11-21 18:34:56 +0100

    chore: Add logger to broker

--- broker/app.py
+++ broker/app.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import FastAPI
 
 from broker.config import Settings, get_settings
@@ -24,18 +26,28 @@ def create_app(
         title=settings.APP_NAME,
     )
 
+    logging.basicConfig(
+        level=logging.DEBUG,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    )
+    logger = logging.getLogger("broker_app_logger")
+
     message_storage = MessageStorage(storage_client)
 
     @broker_app.get("/status")
-    async def status():
+    async def status() -> dict[str, str]:
         return {"status": "ok"}
 
     @broker_app.post("/publish")
-    async def publish(message: TimestampedMessage):
+    async def publish(message: TimestampedMessage) -> dict[str, str]:
+        logger.debug(
+            "Received message: %s in topic %s", message.message, message.filepath
+        )
         if matching_topics := message_storage.get_matching_topics(message.filepath):
             for matching_topic in matching_topics:
                 message_storage.publish(topic=matching_topic, message=message)
                 for msg, callback_url in message_storage.consume(matching_topic):
+                    logger.debug("Sending message %s to %s", msg.message, callback_url)
                     sender_client.send(msg, callback_url)
         else:
             return {"status": "topic_not_found"}
@@ -43,7 +55,10 @@ def create_app(
         return {"status": "published"}
 
     @broker_app.post("/subscribe")
-    async def subscribe(subscription: TopicSubscription):
+    async def subscribe(subscription: TopicSubscription) -> dict[str, str]:
+        logger.debug(
+            "%s subscribed to %s", subscription.callback_url, subscription.topic
+        )
         message_storage.subscribe(subscription)
         return {"status": "subscribed"}
 
